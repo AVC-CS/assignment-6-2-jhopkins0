6957f113a9ada02c88d5444978b5a956d490d025
jhopkins0 <156948029+jhopkins0@users.noreply.github.com>
2024-03-13 19:53:25 +0000

    edit1

--- main.hpp
+++ main.hpp
@@ -21,4 +21,18 @@ int getRandom(void)
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
         int med = (rdnum1 + rdnum2 + rdnum3) / 3;
+
+        int med1 = (rdnum1 - med);
+        int med2 = (rdnum2 - med);
+        int med3 = (rdnum3 - med);
+
+        if ((med1 <= med2) && (med1 <= med3)){
+                return rdnum1;
+        }
+        else if((med2 <= med1) && (med2 <= med3)){
+                return rdnum2;
+        }
+        else{
+                return rdnum3;
+        }
 }
